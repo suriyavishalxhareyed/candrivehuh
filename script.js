ac350952083de0dbe9238fa4c19122b828f2218e
Scrosshareyed <suriya.vishal.d@gmail.com>
2026-04-23 13:17:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("bid").onclick= async function(){
     let num=document.getElementById("iid").value;
-    const responce= await fetch(`/check?age=${age}`);
+    const responce= await fetch(`http://localhost:3000/check?age=${age}`);
     const data =await responce.text();
     document.getElementById("oid").textContent=data;
 }
