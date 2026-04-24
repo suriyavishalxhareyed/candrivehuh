3572b220c2ae8cb74662583f335d6dfcaecb146c
Scrosshareyed <suriya.vishal.d@gmail.com>
2026-04-24 07:09:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("bid").onclick= async function(){
-    let num=document.getElementById("iid").value;
-    const responce= await fetch(`http://localhost:3000/check?age=${age}`);
-    const data =await responce.text();
-    document.getElementById("oid").textContent=data;
-}
